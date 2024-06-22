77ed56f78aac25e6f4ddfc4fe7e8affc6b721954
MuturiIsaac <mbuguaisaac68@gmail.com>
2024-06-22 23:44:16 +0300

    creating functional parts

--- src/main/java/com/game/lanceofdestiny/Player.java
+++ src/main/java/com/game/lanceofdestiny/Player.java
@@ -1,58 +1,33 @@
 package com.game.lanceofdestiny;
 
-import java.awt.Color;
 import java.awt.Graphics;
-import java.io.Serializable;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
-public class Player implements Serializable {
-    private  static final long serialVersionUID = 1L;
+public class Player {
     private int x, y;
-    private int width, height; // Add these instance variables
-    private int health;
-    private int mana;
+    private Image image;
 
     public Player(int x, int y) {
         this.x = x;
         this.y = y;
-        this.width = 50; // Set default width
-        this.height = 50; // Set default height
-        this.health = 100; // Default health value
-        this.mana = 50; // Default mana value
+        this.image = new ImageIcon(getClass().getResource("/resources/heart.png")).getImage();
     }
 
-    // Add these getter methods
+    public void draw(Graphics g) {
+        g.drawImage(image, x, y, null);
+    }
+
+    public void move(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
     public int getX() {
         return x;
     }
 
     public int getY() {
         return y;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-
-    public void draw(Graphics g) {
-        g.setColor(Color.BLUE);
-        g.fillRect(x, y, 50, 50);
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public int getMana() {
-        return mana;
-    }
-
-    public void move(int dx, int dy) {
-        x += dx;
-        y += dy;
     }
 }
